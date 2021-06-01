e7c0a23863e1188017084d606232eab229d13c6c
Yohanr19 <38169280+Yohanr19@users.noreply.github.com>
2021-05-31 21:08:08 -0400

    First Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - Base Apparel coming soon page
+
+Base Apparel Coming Soon coded by YohanR19 
+
+
+*This one was a tough one and quite challenging to finish
+One of my favoritesss proyects so far.
---
+++ script.js
@@ -0,0 +1,46 @@
+let btn = document.getElementsByClassName('btn');
+let erroricon= document.getElementsByClassName('alert-icon-container');
+let input= document.getElementsByClassName('email-input');
+let alerttext= document.getElementsByClassName('alert-text');
+let alertcontanier= document.getElementsByClassName('alert-text-container');
+let form = document.getElementsByClassName('form');
+
+
+form[0].addEventListener('submit', (e)=>
+{
+    if(e.target[0].checkValidity()) {
+         hideAlerts();
+    }
+})
+input[0].addEventListener('invalid', (e)=>
+{
+    e.preventDefault();
+    alerttext[0].innerText = e.target.validationMessage;
+    alertcontanier[0].style.opacity = 1;
+    erroricon[0].style.opacity = 1;
+    console.log('worked');
+})
+btn[0].addEventListener('mouseover', ()=>
+{
+btn[0].style.opacity = 0.7;
+
+})
+btn[0].addEventListener('mouseout', ()=>
+{
+btn[0].style.opacity = 1;
+
+})
+let hideAlerts = function ()
+{
+    alertcontanier[0].style.opacity = 0;
+    erroricon[0].style.opacity = 0 ;
+}
+hideAlerts();
+alertcontanier[0].style.transition = 'opacity 0.2s ease-in';
+erroricon[0].style.transition = 'opacity 0.2s ease-in';
+btn[0].style.transition = 'opacity 0.3s ease-out';
+let showAlerts = function() 
+{
+    alertcontanier[0].style.opacity = 1;
+    erroricon[0].style.opacity = 1; ;
+}
